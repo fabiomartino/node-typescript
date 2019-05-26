bb28fc0558d88c3512b6901ef7ee2ad24c7712e3
Fabio Martino <martinofabio@libero.it>
2019-05-26 20:36:17 +0200

    Init repo

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+// Import
+import chalk from 'chalk';
+
+// Const
+const nts = chalk.yellow( `Node with TypeScript` );
+const hello = ( word: string = 'world' ) =>
+{
+  console.log( `${ chalk.blue( 'Hello' ) } ${ word }!` );
+};
+
+hello( nts );
